5f3797e68e02861a8c3743ffa4ab989447fd23c6
Nathan Broaddus <nathanbroaddus@gmail.com>
2017-09-25 15:44:53 -0400

    [react-flickr-search] persisting state with react-persist

--- src/index.js
+++ src/index.js
@@ -2,18 +2,29 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { compose, createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
+import {persistStore, autoRehydrate} from 'redux-persist';
 // CSS 
 import './main.scss';
 
 // Components
 import App from './containers/app.js';
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+
+const store = createStore(
+   reducers,
+   undefined,
+   compose(
+      applyMiddleware(),
+      autoRehydrate()
+   )
+);
+
+persistStore(store);
 
 render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={ store }>
     <App title="App" />
   </Provider>
   , document.getElementById('root'));
